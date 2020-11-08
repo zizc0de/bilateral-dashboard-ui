e428dce0d623a870074d6ad97a38fef250f9cdb1
Muhammad Abdul Aziz <abdul.aziz@usetada.com>
2020-11-09 00:09:10 +0700

    feat(WIP): homepage

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,24 @@ module.exports = {
   },
   purge: [],
   theme: {
+    backgroundColor: {
+      'dark': '#1D1C2F',
+      'secondary': '#F88A28'
+    },
+    colors: {
+      'default': '#333333',
+      'gray': '#777777',
+      'white': '#FFFFFF'
+    },
+    container: {
+      padding: '35px'
+    },
+    fontFamily: {
+      'proxima': 'Proxima-Regular',
+      'proxima-semi-bold': 'Proxima-SemiBold',
+      'proxima-bold': 'Proxima-Bold',
+      'proxima-light': 'Proxima-Light',
+    },
     extend: {},
   },
   variants: {},
