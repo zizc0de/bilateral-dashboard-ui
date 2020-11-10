9b7cda1e191f8af2621521599fd370fc29b00c0f
Muhammad Abdul Aziz <abdul.aziz@usetada.com>
2020-11-11 00:53:14 +0700

    feat: add bar chart on index page

---
+++ dist/js/page-index.js
@@ -0,0 +1,89 @@
+(function () {
+  var chartProjectCount = document.getElementById("chart-project-count").getContext("2d");
+
+  new Chart(chartProjectCount, {
+    type: "bar",
+    data: {
+      labels: [
+        "Belum proses",
+        "Proses penyiapan RC/FS",
+        "Sudah terdapat RC",
+        "Green Book",
+        "Daftar Kegiatan",
+        "Implementasi",
+        "Selesai",
+      ],
+      datasets: [
+        {
+          label: 'Jumlah Proyek',
+          data: [17, 35, 25, 17, 22, 10, 10],
+          backgroundColor: [
+            "#1C3682",
+            "#F88A28",
+            "#1C3682",
+            "#1C3682",
+            "#1C3682",
+            "#1C3682",
+            "#1C3682",
+          ],
+          barThickness: 20,
+        },
+      ],
+    },
+    options: {
+      scales: {
+        yAxes: [
+          {
+            ticks: {
+              beginAtZero: true,
+            },
+          },
+        ],
+      },
+    },
+  });
+
+  var chartProjectValue = document.getElementById("chart-project-value").getContext("2d");
+
+  new Chart(chartProjectValue, {
+    type: "bar",
+    data: {
+      labels: [
+        "Belum proses",
+        "Proses penyiapan RC/FS",
+        "Sudah terdapat RC",
+        "Green Book",
+        "Daftar Kegiatan",
+        "Implementasi",
+        "Selesai",
+      ],
+      datasets: [
+        {
+          label: 'Jumlah Proyek',
+          data: [17, 35, 25, 17, 22, 10, 10],
+          backgroundColor: [
+            "#1C3682",
+            "#F88A28",
+            "#1C3682",
+            "#1C3682",
+            "#1C3682",
+            "#1C3682",
+            "#1C3682",
+          ],
+          barThickness: 20,
+        },
+      ],
+    },
+    options: {
+      scales: {
+        yAxes: [
+          {
+            ticks: {
+              beginAtZero: true,
+            },
+          },
+        ],
+      },
+    },
+  });
+})();
