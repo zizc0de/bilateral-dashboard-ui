27189b625cf46c95e7fb87fc3b1037bd01417216
Muhammad Abdul Aziz <abdul.aziz@usetada.com>
2020-11-15 12:55:20 +0700

    feat: complete feature table and filter

--- dist/js/page-table.js
+++ dist/js/page-table.js
@@ -7,4 +7,11 @@ $(document).ready(function () {
     })
     .columns.adjust()
     .responsive.recalc();
+
+  
+  $("#btn-filter").on('click', function() {
+    $("#more-filter").toggle();
+    $("#btn-filter").toggleClass('btn-primary', 'btn-default');
+    $("#btn-filter > i").toggleClass('fa-chevron-up', 'fa-chevron-down');
+  });
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,27 +6,28 @@ module.exports = {
   purge: [],
   theme: {
     backgroundColor: {
-      'dark': '#1D1C2F',
-      'secondary': '#F88A28',
-      'soft-gray': '#EBECED',
-      'light-gray': '#F5F8FA',
+      dark: "#1D1C2F",
+      secondary: "#F88A28",
+      "soft-gray": "#EBECED",
+      "light-gray": "#F5F8FA",
     },
     colors: {
-      'default': '#333333',
-      'gray': '#777777',
-      'white': '#FFFFFF'
+      default: "#333333",
+      gray: "#777777",
+      white: "#FFFFFF",
+      "soft-gray": "#A9ACBC",
     },
     container: {
-      padding: '37px'
+      padding: "37px",
     },
     fontFamily: {
-      'proxima': 'Proxima-Regular',
-      'proxima-semi-bold': 'Proxima-SemiBold',
-      'proxima-bold': 'Proxima-Bold',
-      'proxima-light': 'Proxima-Light',
+      proxima: "Proxima-Regular",
+      "proxima-semi-bold": "Proxima-SemiBold",
+      "proxima-bold": "Proxima-Bold",
+      "proxima-light": "Proxima-Light",
     },
     extend: {},
   },
   variants: {},
-  plugins: [],
-}
+  plugins: [require("@tailwindcss/custom-forms")],
+};
