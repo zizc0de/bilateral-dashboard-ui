ceb92ed87f5121c011745e520167ced0d0a6fb67
Muhammad Abdul Aziz <abdul.aziz@usetada.com>
2020-11-25 08:01:41 +0700

    feat: add expand table

--- dist/js/page-table.js
+++ dist/js/page-table.js
@@ -2,16 +2,19 @@ $(document).ready(function () {
   $("#dataTable")
     .DataTable({
       responsive: true,
+      columnDefs: [
+        { responsivePriority: 1, targets: 0 },
+        { responsivePriority: 2, targets: -1 },
+      ],
       searching: false,
       lengthChange: false,
     })
     .columns.adjust()
     .responsive.recalc();
 
-  
-  $("#btn-filter").on('click', function() {
+  $("#btn-filter").on("click", function () {
     $("#more-filter").toggle();
-    $("#btn-filter").toggleClass('btn-primary', 'btn-default');
-    $("#btn-filter > i").toggleClass('fa-chevron-up', 'fa-chevron-down');
+    $("#btn-filter").toggleClass("btn-primary", "btn-default");
+    $("#btn-filter > i").toggleClass("fa-chevron-up", "fa-chevron-down");
   });
 });
