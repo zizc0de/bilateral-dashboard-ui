ae96dad4763e5caa56e105d2d02b0a094ada79fd
Muhammad Abdul Aziz <abdul.aziz@usetada.com>
2020-11-29 15:58:45 +0700

    feat: toggle display datatable column

--- dist/js/page-table.js
+++ dist/js/page-table.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  $("#dataTable")
+  let table = $("#dataTable")
     .DataTable({
       scrollX: true,
       searching: false,
@@ -9,8 +9,16 @@ $(document).ready(function () {
     .responsive.recalc();
 
   $("#btn-filter").on("click", function () {
-    $("#more-filter").toggle();
+    $("#more-filter").toggleClass("hidden", "");
     $("#btn-filter").toggleClass("btn-primary", "btn-default");
     $("#btn-filter > i").toggleClass("fa-chevron-up", "fa-chevron-down");
+  });
+
+  $(".data-toggle").on("click", function (e) {    
+    // Get the column API object
+    let column = table.column($(this).attr("data-column"));
+
+    // Toggle the visibility
+    column.visible(!column.visible());
   });
 });
